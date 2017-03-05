5ccd7c13eb692d5c7685814648a63ac0e3b309cd
Davide Imola <davide.imola@icloud.com>
2017-03-05 14:25:04 +0100

    Initial commit

---
+++ env_interface.ml
@@ -0,0 +1,17 @@
+(*
+* Languages and Compilers - Languages (2016/17)
+* Università di Verona
+*
+* Imola Davide - VR386238
+* Slemer Andrea - VR386253
+*)
+
+# module type ENV =
+    sig
+      type 't env
+      val emptyenv : 't -> 't env
+      val bind : 't env * string * 't -> 't env
+      val bindlist : 't env * (string list) * ('t list) -> 't env
+      val applyenv : 't env * string -> 't
+      exception WrongBindlist
+    end
