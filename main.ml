c2198fc5837578829def08e1a5cd9edd5b974ef1
Davide Imola <davide.imola@icloud.com>
2017-03-05 15:23:44 +0100

    Creating main file of the script.

--- main.ml
+++ main.ml
@@ -5,3 +5,25 @@
 * Imola Davide - VR386238
 * Slemer Andrea - VR386253
 *)
+
+#use "syntax.ml";;
+
+#use "env_interface.ml";;
+#use "env_semantics.ml";;
+open Funenv;;
+
+#use "store_interface.ml";;
+#use "store_semantics.ml";;
+open Funstore;;
+
+#use "stack_interface.ml";;
+#use "stack_semantics.ml";;
+open SemStack;;
+
+#use "stackm_interface.ml";;
+#use "stackm_semantics.ml";;
+open SemMStack;;
+
+#use "eval.ml";;
+#use "operazioni.ml";;
+#use "semantics.ml";;
